bd7374150225705aae8f1e3b9c0abfe364eff3e4
cyrus chen <pschen@lcs.on.ca>
2023-04-20 09:22:59 -0400

    Initial Commit

---
+++ StarWarsSearchEngine/StarWarsSearchEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSearchEngineApp.swift
+//  StarWarsSearchEngine
+//
+//  Created by Cyrus Chen on 20/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsSearchEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
