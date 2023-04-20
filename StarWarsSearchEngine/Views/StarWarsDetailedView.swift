b5abd4319ecc0f2b08f756c9db1bc4f095d0445a
cyrus chen <pschen@lcs.on.ca>
2023-04-20 09:24:51 -0400

    Created project and completed initial organization of views.

---
+++ StarWarsSearchEngine/Views/StarWarsDetailedView.swift
@@ -0,0 +1,20 @@
+//
+//  StarWarsDetailedView.swift
+//  StarWarsSearchEngine
+//
+//  Created by Cyrus Chen on 20/4/2023.
+//
+
+import SwiftUI
+
+struct StarWarsDetailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StarWarsDetailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        StarWarsDetailedView()
+    }
+}
